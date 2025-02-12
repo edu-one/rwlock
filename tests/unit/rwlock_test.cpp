7aed57f5a50ec161585d98b479c555c58afc897c
Denys Valchuk <dvalchuk@gmail.com>
2025-02-12 11:07:54 +0200

    more unit tests

--- tests/unit/rwlock_test.cpp
+++ tests/unit/rwlock_test.cpp
@@ -105,3 +105,100 @@ TEST(RWLock, ReadWriteLockUnlock) {
         t.join();
     }
 }
+
+TEST(RWLock, MultipleReadersConcurrent) {
+    dv::rwlock::RWLock rwlock;
+    std::atomic<size_t> sharedCounter{0};
+    
+    auto reader = [&rwlock, &sharedCounter]() {
+        for (int i = 0; i < 1000; i++) {
+            rwlock.readLock();
+            sharedCounter.load();
+            std::this_thread::sleep_for(std::chrono::microseconds(1));
+            rwlock.readUnlock();
+        }
+    };
+
+    std::vector<std::thread> threads;
+    const size_t numReaders = 20;
+    for (size_t i = 0; i < numReaders; ++i) {
+        threads.emplace_back(reader);
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+}
+
+TEST(RWLock, PreventReaderStarvation) {
+    dv::rwlock::RWLock rwlock;
+    std::atomic<size_t> writeCount{0};
+    std::atomic<bool> running{true};
+
+    auto writer = [&]() {
+        while (running) {
+            rwlock.writeLock();
+            writeCount++;
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            rwlock.writeUnlock();
+        }
+    };
+
+    auto reader = [&]() {
+        size_t localReadCount = 0;
+        while (running) {
+            rwlock.readLock();
+            localReadCount++;
+            std::this_thread::sleep_for(std::chrono::microseconds(100));
+            rwlock.readUnlock();
+        }
+    };
+
+    std::vector<std::thread> readers;
+    for (int i = 0; i < 5; ++i) {
+        readers.emplace_back(reader);
+    }
+
+    std::thread writerThread(writer);
+    
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    running = false;
+
+    writerThread.join();
+    for (auto& t : readers) {
+        t.join();
+    }
+
+    EXPECT_GT(writeCount, 100) << "Writer should have made progress";
+}
+
+TEST(RWLock, ExceptionSafety) {
+    struct Exception : std::exception {};
+    dv::rwlock::RWLock rwlock;
+
+    // Test read lock exception safety
+    try {
+        rwlock.readLock();
+        throw Exception();
+        rwlock.readUnlock();
+    } catch (const Exception&) {
+        rwlock.readUnlock();
+    }
+
+    // Verify lock is still usable
+    rwlock.writeLock();
+    rwlock.writeUnlock();
+
+    // Test write lock exception safety
+    try {
+        rwlock.writeLock();
+        throw Exception();
+        rwlock.writeUnlock();
+    } catch (const Exception&) {
+        rwlock.writeUnlock();
+    }
+
+    // Verify lock is still usable
+    rwlock.readLock();
+    rwlock.readUnlock();
+}
